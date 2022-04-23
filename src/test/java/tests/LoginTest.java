24eb8fa5da85fabc55e078c7f9ae28a591be9c68
syzzana <syzana.kajtazaj@gmail.com>
2022-04-24 00:38:15 +0200

    experiment: cross browser testing selenium

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -2,13 +2,22 @@ package tests;
 
 
 import org.junit.Assert;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import pages.LoginPage;
 import org.junit.Test;
 
 import static utils.Constants.*;
 
+@RunWith(Parameterized.class)
 public class LoginTest extends TestBase {
 
+    @Parameterized.Parameters
+    public static String [] browsers() {
+        return new String [] {"firefox", "chrome"};
+    }
+
+    //this test should run 2 times
     @Test
     public void verifyLogin()  {
         LoginPage loginPage = new LoginPage(driver);
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,25 +3,43 @@ package tests;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-//import org.testng.annotations.BeforeSuite;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.junit.BeforeClass;
-import org.junit.AfterClass;
-//import org.junit.BeforeSuite;
+
+import static utils.Constants.BASE_URL;
 
 abstract class TestBase {
 
     protected WebDriver driver;
     @BeforeClass
-    public static void setupSuite() {
-        WebDriverManager.chromedriver().setup();
+    @Parameterized.Parameters( name = "browser")
+    public static void setupSuite(String browser) {
+        if(browser.equalsIgnoreCase("firefox")){
+            WebDriverManager.firefoxdriver().setup();
+        }
+        else if(browser.equalsIgnoreCase("chrome")){
+            WebDriverManager.chromedriver().setup();
+        }
     }
 
     @Before
-    public void setup() {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+    @Parameterized.Parameters(name = "browser")
+    public void setup(String browser) {
+        if(browser.equalsIgnoreCase("firefox")){
+            driver = new FirefoxDriver();
+            driver.manage().window().maximize();
+            driver.navigate().to(BASE_URL);
+            driver.manage().deleteAllCookies();
+        }
+        else if(browser.equalsIgnoreCase("chrome")){
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.navigate().to(BASE_URL);
+            driver.manage().deleteAllCookies();
+        }
      }
 
     @After
